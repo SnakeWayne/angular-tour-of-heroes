810ece067f23fac3b37414042d693b10b52a6371
SnakeWayne <526588851@qq.com>
2020-03-01 15:37:45 +0800

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild,ElementRef,AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-tour-of-heroes';
+export class AppComponent  {
+  /** Template reference to the canvas element */
+  title = 'Tour of Heroes';
+
 }
